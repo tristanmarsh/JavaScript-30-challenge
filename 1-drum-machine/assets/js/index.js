3c33bf8ecde98c5225d81df708e9889f69c23468
Tristan Marsh <tristanmarsh@live.com>
2018-06-18 20:30:35 +1000

    New: Drum Machine

---
+++ 1-drum-machine/assets/js/index.js
@@ -0,0 +1,27 @@
+(function() {
+    const handleKeyPress = e => {
+        playSound(e);
+        animateKey(e);
+    };
+
+    const playSound = e => {
+        const sound = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+        if (sound) {
+            sound.currentTime = 0;
+            sound.play();
+        }
+    };
+
+    const animateKey = e => {
+        const key = document.querySelector(`.key[data-key='${e.keyCode}']`);
+        if (key) {
+            key.classList.add('playing');
+            key.addEventListener('transitionend', e => {
+                if (e.propertyName == 'transform') {
+                    key.classList.remove('playing');
+                }
+            });
+        }
+    };
+    window.addEventListener('keydown', handleKeyPress);
+})();
