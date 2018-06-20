a829d30f27e99df2ea23d363ee3a8bfc87217751
Tristan Marsh <tristanmarsh@live.com>
2018-06-20 23:06:32 +1000

    New: Day 3 CSS Variables

---
+++ 3-css-variables/assets/js/index.js
@@ -0,0 +1,14 @@
+{
+    const inputs = document.querySelectorAll('.controls input');
+
+    const handleChange = (e) => {
+        const name = e.target.name;
+        const value = e.target.value;
+        const sizing = e.target.dataset.sizing || '';
+        document.documentElement.style.setProperty(`--${name}`, `${value}${sizing}`);
+    }
+
+    inputs.forEach(input => input.addEventListener('change', handleChange));
+    inputs.forEach(input => input.addEventListener('mousemove', handleChange));
+    inputs.forEach(input => input.dispatchEvent(new Event('change')));
+}    
