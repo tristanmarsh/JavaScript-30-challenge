d95a1b65cf79f96b2a207298166fddcb3756246a
Tristan Marsh <tristanmarsh@live.com>
2018-06-27 20:23:06 +1000

    Update: Unsplashify
    + clean up JS

--- 10-hold-shift-check-boxes/assets/js/index.js
+++ 10-hold-shift-check-boxes/assets/js/index.js
@@ -1,41 +1,29 @@
 {
-  const app = {
-    shiftActive: false,
-    lastClick: null,
-  };
+  let lastClick = null;
 
-  const shiftSelect = (fromIndex, toIndex, parent) => {
-    if (fromIndex < toIndex) {
-      for (let i = fromIndex; i <= toIndex; i++) {
-        parent.children[i].querySelector('input').checked = true;
-      }
-    } else {
-      for (let i = toIndex; i <= fromIndex; i++) {
-        parent.children[i].querySelector('input').checked = true;
-      }
+  const selectRange = (fromIndex, toIndex, parent) => {
+    let floor, ceiling;
+    [floor, ceiling] = (fromIndex <= toIndex)
+      ? [fromIndex, toIndex]
+      : [toIndex, fromIndex];
+
+    for (let i = floor; i <= ceiling; i++) {
+      parent.children[i].querySelector('input').checked = true;
     }
   };
 
   const handleInboxClick = e => {
     const target = e.target.classList.contains('item') ? e.target : e.target.closest('.item');
-
     const targetIndex = Array.prototype.indexOf.call(target.parentElement.children, target);
 
-    if (app.shiftActive) {
-      shiftSelect(app.lastClick, targetIndex, target.parentElement);
+    if (e.shiftKey) {
+      selectRange(lastClick, targetIndex, target.parentElement);
     } else {
-      app.lastClick = null;
+      lastClick = null;
     }
 
-    // leave trail for next click
-    if (!app.shiftActive) {
-      app.lastClick = null;
-    }
-    app.lastClick = targetIndex;
-    app.shiftActive = e.shiftKey;
+    lastClick = targetIndex;
   };
-
-  document.body.addEventListener('keypress', (e) => window.console.log(e));
 
   const inbox = document.querySelector('.inbox');
   inbox.addEventListener('click', handleInboxClick);
