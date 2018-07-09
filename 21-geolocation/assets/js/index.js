9438a3ad4556fea1392a0f9b27d0a64cd5b97b79
Tristan Marsh <tristanmarsh@live.com>
2018-07-09 23:12:53 +1000

    feat(21-geolocation): Add Latitude, Longitude and Maps Link

--- 21-geolocation/assets/js/index.js
+++ 21-geolocation/assets/js/index.js
@@ -1,10 +1,20 @@
 {
   const arrow = document.querySelector('.arrow');
   const speed = document.querySelector('.speed-value');
+  const lat = document.querySelector('.lat');
+  const long = document.querySelector('.long');
+  const mapsLink = document.querySelector('.maps-link');
 
   navigator.geolocation.watchPosition((data) => {
     window.console.log(data);
     speed.textContent = data.coords.speed;
+
+    lat.textContent = data.coords.latitude;
+    long.textContent = data.coords.longitude;
+
+    mapsLink.href = `https://www.google.com/maps/@-${data.coords.latitude},${data.coords.longitude},14z`;
+    mapsLink.textContent = 'Maps Please';
+
     arrow.style.transform = `rotate(${data.coords.heading}deg)`;
   }, (err) => {
     window.console.error(err);
