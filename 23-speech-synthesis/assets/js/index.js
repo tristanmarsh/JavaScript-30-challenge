b95b6d3959f2ebec33107fc8b44060670df9e535
Tristan Marsh <tristanmarsh@live.com>
2018-07-10 22:48:56 +1000

    New: Day 23 - Speech Synthesis

---
+++ 23-speech-synthesis/assets/js/index.js
@@ -0,0 +1,44 @@
+{
+  const msg = new SpeechSynthesisUtterance();
+  let voices = [];
+  const voicesDropdown = document.querySelector('[name="voice"]');
+  const options = document.querySelectorAll('[type="range"], [name="text"]');
+  const speakButton = document.querySelector('#speak');
+  const stopButton = document.querySelector('#stop');
+
+  function populateVoices() {
+    voices = this.getVoices();
+    const voiceOptions = voices
+      .filter(voice => voice.lang.includes('en'))
+      .map(voice => `<option value=${voice.name}">${voice.name} (${voice.lang})</option>`).join('');
+    voicesDropdown.innerHTML = voiceOptions;
+  }
+
+  function toggle() {
+    speechSynthesis.cancel();
+    speechSynthesis.speak(msg);
+  }
+
+  function setVoice() {
+    msg.voice = voices.find(voice => voice.name === this.value);
+    toggle();
+  }
+
+  function stopSpeech() {
+    speechSynthesis.cancel();
+  }
+
+  function setOption() {
+    msg[this.name] = this.value;
+    toggle();
+  }
+
+  (function initApp() {
+    msg.text = document.querySelector('[name="text"]').value;
+    speechSynthesis.addEventListener('voiceschanged', populateVoices);
+    voicesDropdown.addEventListener('change', setVoice);
+    speakButton.addEventListener('click', toggle);
+    stopButton.addEventListener('click', stopSpeech);
+    options.forEach(option => option.addEventListener('change', setOption));
+  }());
+}
